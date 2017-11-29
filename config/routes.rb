334444a532f9df1263cac5066ab78622b3e46dde
nhatquang <vonhatquang100@gmail.com>
2017-11-29 17:56:24 +0700

    Design admin template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "/signup", to: "users#new"
+  get "/admin/user", to: "admin#index"
   resources :users
+  namespace :admin do
+    resources :users
+  end
 end
